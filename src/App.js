8c17fa3284e2fd21df91710f82e9fe235fddfb7e
sh3ldonn <58163083+sh3ldonn@users.noreply.github.com>
2023-05-31 23:31:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies();
+    searchMovies("Batman");
   }, []);
 
   const searchMovies = async (title) => {
